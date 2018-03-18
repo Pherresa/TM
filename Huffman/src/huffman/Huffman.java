53a5e2b51628a92e062715462c7a7bd24824ac96
Pablo <pherresa7@alumnes.ub.edu>
2018-03-18 16:42:07 +0100

    Improve argument control

--- Huffman/src/huffman/Huffman.java
+++ Huffman/src/huffman/Huffman.java
@@ -5,6 +5,9 @@
  */
 package huffman;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author pablo
@@ -18,12 +21,31 @@ public class Huffman {
         
         //Comprobar entrada datos es de tipo 2xN
         int esImpar = args.length%2;
+        System.out.println(args.length);
         //Correcto
         if(esImpar == 0){
             System.out.println("Tabla entrada es correcta");
-        //Incorrecto
-        }else
+            List<String> simbols = new ArrayList<>();
+            List<Integer> porcentajes = new ArrayList<>();
+            for(int i = 0; i < args.length; i++){
+                //porcentajes
+                if(i%2 == 0){
+                    System.out.println(args[i]);
+                    simbols.add(args[i]);
+                //Porcentajes    
+                }else{
+                    System.out.println(Integer.valueOf(args[i]));
+                    porcentajes.add(Integer.valueOf(args[i]));
+                }
+            }
+            huffman(simbols, porcentajes);
+        }
+        else
             System.out.println("Tabla entrada no es correcta");
         }
+
+    private static void huffman(List<String> simbols, List<Integer> porcentajes) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
