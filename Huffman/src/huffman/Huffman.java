4c7d77837e1989aa37889959468e4c34bdd4dcaa
Pablo <pherresa7@alumnes.ub.edu>
2018-03-18 20:39:12 +0100

    Comentado siguiente paso algoritmo
    He añadido a pelo la entrada por si nos hiciese falta, he dejado
    comentado como empezar a crear el árbol desde abajo, pero no tengo claro
    como acceder al diccionario y luego crear el siguiente nivel del árbol
    :-1:

--- Huffman/src/huffman/Huffman.java
+++ Huffman/src/huffman/Huffman.java
@@ -6,6 +6,10 @@
 package huffman;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -18,34 +22,29 @@ public class Huffman {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        String[][] simbolos = new String[][]{{"D","30"},
+                                             {"K","20"},
+                                             {"Q","2"},
+                                             {"J","15"},
+                                             {"10","10"},
+                                             {"9","5"}
+                                            };
         
-        //Comprobar entrada datos es de tipo 2xN
-        int esImpar = args.length%2;
-        System.out.println(args.length);
-        //Correcto
-        if(esImpar == 0){
-            System.out.println("Tabla entrada es correcta");
-            List<String> simbols = new ArrayList<>();
-            List<Integer> porcentajes = new ArrayList<>();
-            for(int i = 0; i < args.length; i++){
-                //porcentajes
-                if(i%2 == 0){
-                    System.out.println(args[i]);
-                    simbols.add(args[i]);
-                //Porcentajes    
-                }else{
-                    System.out.println(Integer.valueOf(args[i]));
-                    porcentajes.add(Integer.valueOf(args[i]));
-                }
-            }
-            huffman(simbols, porcentajes);
-        }
-        else
-            System.out.println("Tabla entrada no es correcta");
-        }
+        huffman(simbolos);
 
-    private static void huffman(List<String> simbols, List<Integer> porcentajes) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    private static void huffman(String[][] simbols) {
+        
+        Collections.sort(porcentajes);
+        Collections.reverse(porcentajes);
+        int derecha, izquierda;
+        HashMap<Integer, String> resultado = new HashMap();
+        izquierda = porcentajes.get(porcentajes.size()-1);
+        derecha = porcentajes.get(porcentajes.size()-2);
+        
+        
+        System.out.println(porcentajes.get(0));
     }
+
+    
     
 }
