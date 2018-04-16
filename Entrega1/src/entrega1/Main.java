7e88a8e1b69a94b0f3d11598b444f70812ee9663
Pablo <pherresa7@alumnes.ub.edu>
2018-04-16 20:50:06 +0200

    Jcommander añadido al main

--- Entrega1/src/entrega1/Main.java
+++ Entrega1/src/entrega1/Main.java
@@ -5,17 +5,34 @@
  */
 package entrega1;
 
+import entrega1.Parser.ArgumentParser;
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.ParameterException;
+
 /**
  *
  * @author pablo
  */
 public class Main {
 
+    ArgumentParser args;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        ArgumentParser arguments = new ArgumentParser();
+        try{
+            JCommander jc = new JCommander(arguments, args);
+            Main main2 = new Main(arguments);
+            
+        }catch(ParameterException e){
+            System.out.println("Error : " + e.getMessage());
+        }
+    }
+
+    private Main(ArgumentParser arguments) {
+        this.args = arguments;
+        //TODO ABRIR ZIP, LEER IMAGENES Y APLICAR FILTROS.
     }
     
 }
