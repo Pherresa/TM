9889e9388c550fb632735048cbc5caea14330a97
Chan yong Yoon <chanyongyoon92@gmail.com>
2018-04-19 18:40:10 +0200

    Output a zip hecho

--- Entrega1/src/entrega1/Main.java
+++ Entrega1/src/entrega1/Main.java
@@ -51,7 +51,7 @@ public class Main {
         
         // Para poder mostrar a los fps que pida user.
         long time_before, time_after;
-        int fps = 10; // AHora mismo imprimo cada 1 segundo.
+        int fps = 1; // AHora mismo imprimo cada 1 segundo.
         int ms_sleep;
        
         BufferedImage image; // Imagen a mostrar.
@@ -60,14 +60,13 @@ public class Main {
         JFrame frame = new JFrame(); // Ventana donde se mostrará la imagen.
         JLabel jlabel = new JLabel(); // Etiqueta donde metemos imagen y este metemos en la ventana.
         ZipFile zf; // Para abrir archivos .zip
-        ZipEntry entry;
-        ZipOutputStream zous;
+        ZipOutputStream zout;
                 
         try{
             // Abrimos archivo zip de donde cogemos imagenes.
             zf = new ZipFile(this.args.getInput());
             // Zip donde guardaremos las imagenes.
-            zous = new ZipOutputStream(new FileOutputStream(this.args.getOutput()));
+            zout = new ZipOutputStream(new FileOutputStream(this.args.getOutput()));
             
             // Convertimos en "lista" la entrada de imagenes.
             Enumeration<? extends ZipEntry> entries = zf.entries();
@@ -76,13 +75,12 @@ public class Main {
             int i = 0; // variable de prueba.
             while(entries.hasMoreElements()) {               
                 // Obtenemos una imagen.
-                entry = entries.nextElement();
-                image = ImageIO.read(zf.getInputStream(entry));
+                image = ImageIO.read(zf.getInputStream(entries.nextElement()));
                 
                 //** APLICAMOS FILTROS **//
                 
                 //image = filtre.binaritzacio(image, 70);
-                //image = filtre.negatiu(image);
+                image = filtre.negatiu(image);
                 //image = filtre.averaging(image, 3);
                 
                 // Estos dos son opcionales, solo quiero probar.
@@ -94,14 +92,13 @@ public class Main {
                 images.add(image); // Añadimos a la lista para despues reproducir.
                 
                 //** GUARDAMOS IMAGEN EN .zip **//
-                // TODO
-                // zous.putNextEntry(new ZipEntry("result"+i+".jpeg"));
-                // ImageIO.write(image, "jpeg", zous);
+                zout.putNextEntry(new ZipEntry("result"+i+".jpeg"));
+                ImageIO.write(image, "jpeg", zout);
                 
                 i++;
             }
             // Cerramos zips, tanto el de entrada como salida.
-            //zou.close();
+            zout.close();
             zf.close();
             
             //** REPRODUCIMOS VIDEO. **//
@@ -111,8 +108,9 @@ public class Main {
                 
                 time_before = System.currentTimeMillis();
                 
-                jlabel.setIcon(new ImageIcon(img)); // Añadimos la imagen al label.
-                frame.getContentPane().add(jlabel);
+                // Metemos al frame la imagen a mostrar.
+                jlabel.setIcon(new ImageIcon(img));
+                frame.add(jlabel);
                 frame.pack();
                 
                 // Calculamos si hay que hacer un sleep o no.
